0c48c9a2165ffd4535055a17ee106ee6e6c73b0f
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-07-04 14:33:26 +0300

    Adds js Updates

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+// Write your code 
+const main = document.getElementById(`main`);
+main.remove();
+const newHeader = document.createElement(`h1`);
+newHeader.setAttribute(`id`, `victory`)
+newHeader.innerHTML = newHeader.textContent =`Nathan is the champion`;
+document.body.append(newHeader);
